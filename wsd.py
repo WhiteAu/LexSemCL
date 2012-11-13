dd19bab16bb53f7e6d5755b27066dae82d8b67cd
Scott <runscottrun@gmail.com>
2012-11-13 13:28:07 -0500

    implemented levenshtein distance. check feat name

--- wsd.py
+++ wsd.py
@@ -91,11 +91,30 @@ def complexFFeatures(doc, i, j):
 def complexPairFeatures(doc, i, j, ew, wprob):
     feats = Counter()
     
+    ###String similarity goes here
+    feats['w_dist'] = levenshtein(ew, doc[i][j])
+    
     ###context disambiguation goes here
         
         
     return feats
+    
+# Levenshtein distance: test this
+def levenshtein(s1, s2):
+    l1 = len(s1)
+    l2 = len(s2)
 
+    matrix = [range(l1 + 1)] * (l2 + 1)
+    for zz in range(l2 + 1):
+      matrix[zz] = range(zz,zz + l1 + 1)
+    for zz in range(0,l2):
+      for sz in range(0,l1):
+        if s1[sz] == s2[zz]:
+          matrix[zz+1][sz+1] = min(matrix[zz+1][sz] + 1, matrix[zz][sz+1] + 1, matrix[zz][sz])
+        else:
+          matrix[zz+1][sz+1] = min(matrix[zz+1][sz] + 1, matrix[zz][sz+1] + 1, matrix[zz][sz] + 1)
+    return matrix[l2][l1]
+    
 if __name__ == "__main__":
     (train_acc, test_acc, test_pred) = runExperiment('Science.tr', 'Science.de', simpleFFeatures, simpleEFeatures, simplePairFeatures, quietVW=True)
     print 'training accuracy =', train_acc
